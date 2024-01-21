eae8efd9dcea227525aa0b371bd1a8f7923c872e
Satyik Das <satyikdas816@gmail.com>
2024-01-21 15:58:09 +0530

    uncommented send_email code

--- src/send_mail.py
+++ src/send_mail.py
@@ -33,11 +33,11 @@ def send_email(receiver_email, subject, msg):
 
     # Attach the HTML content to the email
     message.attach(MIMEText(html, 'html'))
-    # context = ssl.create_default_context()
-    # with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    #     server.set_debuglevel(1)
-    #     server.login(sender_email, password)
-    #     server.sendmail(sender_email, receiver_email, message.as_string())
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.set_debuglevel(1)
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
     conn = get_db()
     cur = conn.cursor()
     cur.execute('''
